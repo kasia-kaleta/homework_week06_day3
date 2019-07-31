9e9059c1c7b7f32b94b28af701a6358b102a3804
kat3005 <kasia.alicja@gmail.com>
2019-07-31 18:54:34 +0100

    Adds completed MVP

--- models/traveller.js
+++ models/traveller.js
@@ -15,20 +15,25 @@ Traveller.prototype.getJourneyEndLocations = function () {
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+      return total += journey.distance;
+    }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  
 };
-
 
 module.exports = Traveller;
